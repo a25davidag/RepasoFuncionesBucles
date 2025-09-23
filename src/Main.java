758f0ad2b54bfff59b7ed6bec2b67277c91f87f4
dabid chu <arranzd5@gmail.com>
2025-09-24 00:57:25 +0200

    terminado casa

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Ejercicio1
-        /*Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int numeroPIstas;
         do {
             System.out.println(" Introduce el numero de pista ");
@@ -16,7 +16,7 @@ public class Main {
             } else {
                 System.out.println(" Numero de pista incorrecto");
             }
-        } while (numeroPIstas != 0);*/
+        } while (numeroPIstas != 0);
 
         //Ejercicio 2
         Scanner st = new Scanner(System.in);
@@ -30,15 +30,69 @@ public class Main {
         } else if (comprobarNivelOxigeno < 40) {
             System.out.println(" critico");
         }
+        //Ejercicio3
+        recolectarMeteoritos(10);
+        //Ejercicio 4
+        Scanner sm = new Scanner(System.in);
+        String codigoSatelite;
+        System.out.println("=== SIstema de busqueda de satelites ===");
+        System.out.println(" Ingrese el codigo de satelite (SAT-A, SAT-B, SAT-C, SAT-D, SAT-E)");
+        System.out.println("Escribe 'FIN' para salir. ");
+
+        while(true){
+            System.out.println(" Ingresa el codigo del satélite: ");
+            codigoSatelite = sm.next().toUpperCase();
+            if(codigoSatelite.equals("FIN")){
+                System.out.println(" Salir del satelite");
+                break;
+            }
+        buscarSatelites(codigoSatelite);
+        }
+
+
+
+
+
+
+
+
+
+
+
     }//Ejercicio 2
-    public static int comprobarNivelOxigeno ( int x){
+    public static int comprobarNivelOxigeno(int x) {
         for (int i = 1; i <= 3; i++) {
             System.out.println(" Ingrese el nivel de oxigeno (" + i + "/3): ");
         }
         return 0;
     }
+
     //Ejercicio 3
-
-
-
+    public static int recolectarMeteoritos(int n) {
+        for (int i = 1; i <= n; i++) {
+            int progreso = (i * 100) / n;
+            System.out.println(" Meteorito" + i + " de " + n + " recogido. Progreso: " + progreso + "%");
+        }
+        System.out.println(" Mision de recoleccion completado");
+        return 0;
+    }
+    //Ejercicio 4
+    public static void buscarSatelites(String x){
+        switch (x){
+            case "SAT-A":
+            case "SAT-B":
+            case "SAT-C":
+                System.out.println("Satelite activo, transmitiendo datos");
+                break;
+            case "SAT-D":
+            case "SAT-E":
+                System.out.println("Satelite inactivo, requiere mantenimiento");
+                break;
+            default:
+                System.out.println("Satelite desconocido");
+        }
+    }
 }
+
+
+
