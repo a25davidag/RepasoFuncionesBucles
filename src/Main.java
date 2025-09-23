87562ce55d6740d8f24b92732e117d47577cb8d9
David Arranz González <a25davidag@teis.intranet>
2025-09-23 17:16:06 +0200

    repaso1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Ejercicio1
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Introduce el numero de pista ");
+        System.out.print(" Pon el numero de pista ");
+        int pistas = sc.nextInt();
+        numeroPistas(pistas);
         /*Scanner sc = new Scanner(System.in);
         int numeroPIstas;
 
@@ -23,22 +29,42 @@ public class Main {
 
         //Ejercicio 2
         Scanner st = new Scanner(System.in);
+        System.out.println("Pon un nivel de oxigeno");
         int comprobarNivelOxigeno = st.nextInt();
-        //comprobarNivelOxigeno(comprobarNivelOxigeno);
-
-        if (comprobarNivelOxigeno >= 80 && comprobarNivelOxigeno <= 100){
+        comprobarNivelOxigeno(comprobarNivelOxigeno);
+        if (comprobarNivelOxigeno >= 80 && comprobarNivelOxigeno <= 100) {
             System.out.println("nivel oxigeno optimo");
         } else if (comprobarNivelOxigeno >= 40 && comprobarNivelOxigeno <= 79) {
             System.out.println(" Rebastecimiento");
         } else if (comprobarNivelOxigeno < 40) {
             System.out.println(" critico");
         }
+    }//Ejercicio 1
+        public static void numeroPistas(int x){
+            do {
+                /*System.out.println(" Introduce el numero de pista ");
+                System.out.print(" Pon el numero de pista ");*/
+                if (x >= 1 && x <= 5) {
+                    System.out.println(" El aterrizaje es seguro");
+                } else if (x == 0) {
+                    System.out.println(" El aterrrizaje debe cancelarse");
+                }else{
+                    System.out.println(" Numero de pista incorrecto");
+                }
 
-public static void comprobarNivelOxigeno (int x){
-            for (int i = 1; i <= 3; i++){
-                System.out.println(" Ingrese el nivel de oxigeno (" + i + "/3): ");
-                if
-            }
+
+            } while (x  != 0);
         }
+
+
+        public static int comprobarNivelOxigeno ( int x){
+            for (int i = 1; i <= 3; i++) {
+                System.out.println(" Ingrese el nivel de oxigeno (" + i + "/3): ");
+            }
+            return 0;
+        }
+
+
     }
-}
+
+
